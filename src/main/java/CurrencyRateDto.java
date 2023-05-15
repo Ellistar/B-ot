4fe8275ecb1322d3e3ae2a73dd146c32753c3713
Ellistar <117102466+Ellistar@users.noreply.github.com>
2023-05-16 00:22:49 +0300

    Create CurrencyRateDto.java

---
+++ src/main/java/CurrencyRateDto.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CurrencyRateDto {
+    private Currency currency;
+    private BigDecimal buyRate;
+    private BigDecimal sellRate;
+}
